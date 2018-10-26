ccbd1bd227661f263ae4c4f47f5b94a815eab65c
Justin Schieck <schieck91@gmail.com>
2018-10-26 06:52:24 -0400

    design start for header and footer (#13)

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Wrapper = styled.div`
+    display: block;
+    height: 40px;
+    background-color: red;
+`;
+
+const Footer = () => {
+	return (		
+        <Wrapper>Footer</Wrapper>
+	);
+};
+
+export default Footer;
