3ca921d8f3e73788dae880ffb5572d45575141e6
Justin Schieck <schieju@americas.manulife.net>
2018-11-02 19:27:15 -0400

    added from our colab session

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import styled from 'styled-components';
 import Board from './components/Board';
 import Header from './components/Header';
 import Footer from './components/Footer';
+
+import { Modal } from 'react-bootstrap';
 import randomName from 'random-name';
 
 const Wrapper = styled.div`
@@ -20,11 +22,14 @@ const Wrapper = styled.div`
 	margin-top: 100px;	
 `;
 
+const ModalWrap = styled.div`
+`;
+
 const socket = io.connect('35.182.139.132:3000');
 
 const genTitle = () => {
 	const titles = ['DJ', 'Señor', 'Sir', 'Sire', 'Your Grace', 'Master', 'The Honorable', 'Doctor', 'Captain', 'Colonel', 'Madam', 'Princess', 'Prince', 'Humperdink', 'Mayor', 'Security Chief', 'Darth', 'First Officer', 'Sheriff', 'Coach']
-	const rand = Math.floor( Math.random() * 20 );
+	const rand = Math.floor( Math.random() * titles.length);
 	return titles[rand];
 };
 
@@ -34,9 +39,9 @@ export default class App extends Component {
 		this.state = {
 			userName: '',
 			query: '',
+			show: false,
 		};
 	}
-
 
 	componentDidMount(){
 		this.setState({
@@ -44,20 +49,79 @@ export default class App extends Component {
 		});
 	}
 
+	handleClose = () => {
+		this.setState({ show: false });
+	}
+	
 	getQuery = (data) => {
 		this.setState({
 			query: data
 		});
 	}
 
+	exportData = () => {
+		const ActionItems = [
+			{
+				text: 'Hi',
+				id: 1,
+				comments:['Boo you suck'],
+				vote: 3,
+				isFavourite: true,
+				user: 'DJ Joe Blow'
+			},
+			{
+				text: 'MP rocks',
+				id: 2,
+				comments:['Yay me'],
+				vote: 3,
+				isFavourite: true,
+				user: 'Captian Jim Blow'
+			},
+			{
+				text: 'Transactional Logging Blows',
+				id: 3,
+				comments:['This comment is invalid'],
+				vote: 3,
+				isFavourite: true,
+				user: 'Micheal'
+			},
+		]
+
+		const ArrayCards = ActionItems.reduce((accum, e)=>{ 
+			const cardText = `
+				### Card Title
+				${e.text}
+				votes: ${e.vote}
+			`;
+			return accum + cardText;
+		 }, '');
+
+		this.setState({ show: true })
+		//  const blob = new Blob([ArrayCards], {type: 'text/markdown'});
+		//  const uri = URL.createObjectURL(blob);
+		//  window.open(uri);
+		// socket.emit('Export Data', )
+	} 
+
 	render() {
-		const { userName, query } = this.state
-		console.log('App data: ', query)
+		const { userName, query, show } = this.state
+		console.log('App data: ', query);
+
 		return (
 			<SocketProvider socket={socket}>
-				<Header sendQuery={this.getQuery} userName={userName}/>
+				<Header handleExport={this.exportData} sendQuery={this.getQuery} userName={userName}/>
 				<Wrapper className="App">  					
 					<Board searchContent={query} userName={userName} style={{ flexGrow: 1 }} />
+					<ModalWrap className="static-modal">
+						<Modal.Dialog show={show}>
+							<Modal.Header>
+								<Modal.Title>Modal title</Modal.Title>
+							</Modal.Header>
+							<Modal.Footer>
+								<button onClick={this.handleClose}>Close</button>
+							</Modal.Footer>
+						</Modal.Dialog>	
+					</ModalWrap>					
 					<Footer/>
 				</Wrapper>
 			</SocketProvider>
